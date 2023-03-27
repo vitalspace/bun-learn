8f00551104929620be4b2621634e2d626d6e9033
vitalspace <myglobalsounds@gmail.com>
2023-03-26 21:21:09 -0600

    Init

---
+++ design patterns/module.ts
@@ -0,0 +1,31 @@
+/**
+ * Defines an interface named `Module` that has a `private` property of type `string`.
+ */
+interface Module {
+  private: string;
+}
+
+/**
+ * Defines an anonymous function that defines a `privMessage` variable that is an object with a `private`
+ * property with the value "Hello world this is a private message". It also defines a function named
+ * `sayPrivate` that logs the value of the `private` property of `privMessage` to the console.
+ *
+ * Returns an object that has a public function named `sayPublic` that calls `sayPrivate`.
+ */
+const myModule = (() => {
+  const privMessage: Module = {
+    private: "Hello world this is a private message",
+  };
+
+  const sayPrivate = () => console.log(privMessage.private);
+
+  return {
+    sayPublic: () => sayPrivate(),
+  };
+})();
+
+/**
+ * Calls the `sayPublic` function on the `myModule` object, which in turn calls the `sayPrivate` function
+ * and logs the value of the `private` property of `privMessage` to the console.
+ */
+myModule.sayPublic();
